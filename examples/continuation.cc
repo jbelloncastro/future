94d4701e269ab9c4acfc28fe697eac6ac78f626d
Jorge Bellon Castro <jbelloncastro@gmail.com>
2018-12-01 13:09:57 +0000

    Add new micro-benchmark

--- examples/continuation.cc
+++ examples/continuation.cc
@@ -30,5 +30,26 @@ static void BM_ChainSolvedPromise(benchmark::State& state) {
 }
 BENCHMARK(BM_ChainSolvedPromise);
 
+
+static void BM_ChainUnsolvedPromise(benchmark::State& state) {
+    for (auto _ : state) {
+        promise<int> p;
+        future<int>  f = p.get_future();
+        for( unsigned i = 0; i < state.range(0); i++ ) {
+            f = f.then([](int i) -> int {
+                    return i;
+                });
+        }
+        p.set_value(55);
+        if( f.get() != 55 )
+            throw std::runtime_error("Retrieved incorrect value");
+    }
+}
+BENCHMARK(BM_ChainSolvedPromise)
+    ->Args({10})
+    ->Args({1000})
+    ->Args({1000000})
+    ->Args({1000000000});
+
 BENCHMARK_MAIN();
 
